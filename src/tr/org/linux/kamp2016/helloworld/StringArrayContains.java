9a85dcd393036dd15e22d2411775f0176ec3d90b
gizegizem <yesildalgizem@gmail.com>
2016-08-08 20:43:46 +0300

    first commit

--- src/tr/org/linux/kamp2016/helloworld/StringArrayContains.java
+++ src/tr/org/linux/kamp2016/helloworld/StringArrayContains.java
@@ -11,18 +11,19 @@ public class StringArrayContains {
 		String strFind2="September";
 		boolean contains=false;
 		
-		for(int i=0;i<strMonths.length;i++){
+		/*for(int i=0;i<strMonths.length;i++){
 			if(strMonths[i].equals(strFind1)){
 				contains=true;
 				break;
 			}
-		}
+		}*/
 		if(contains){
 			System.out.println("String array Stringi içeriyor "+strFind1);
 		}
 		else
 			System.out.println("String array Stringi içeriyor "+strFind1);
-		contains=Arrays.asList(strMonths).contains(strFind1);
+		contains=Arrays.asList(strMonths).contains(strFind1);//asList olayı array sınıfına özgü bi şey görevi 
+															//strMonths arrayini getirmeye yarıyor
 		System.out.println("Array stringi içeriyor mu? "+strFind1+" "+contains);
 		contains=Arrays.asList(strMonths).contains(strFind2);
 		System.out.println("Array stringi içeriyor mu? "+strFind2+" "+contains);
