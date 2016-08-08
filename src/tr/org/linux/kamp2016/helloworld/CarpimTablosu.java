29cec704034f335b5b7617577d0c5413ae0c1219
gizegizem <yesildalgizem@gmail.com>
2016-08-08 14:39:43 +0300

    first commit

--- src/tr/org/linux/kamp2016/helloworld/CarpimTablosu.java
+++ src/tr/org/linux/kamp2016/helloworld/CarpimTablosu.java
@@ -1,18 +1,29 @@
 package tr.org.linux.kamp2016.helloworld;
 
-public class CokboyutluArray {
+public class CarpimTablosu {
 
 	public static void main(String[] args) {
-		int[][] board = new int[4][4];
-		int counter = 0;
+		int[][] multitable = initializeMultiplicationTable(10);
+		arayToPrint(multitable);
+
+	}
+
+	public static int[][] initializeMultiplicationTable(int size) {
+		int[][] board = new int[size][size];
+
 		for (int i = 0; i < board.length; i++) {
 
 			for (int j = 0; j < board[i].length; j++) {
-				board[i][j] = counter;
-				counter++;
-			}
-		}
+				board[i][j] = (i + 1) * (j + 1);
+				;
 
+			}
+
+		}
+		return board;
+	}
+
+	public static void arayToPrint(int[][] board) {
 		for (int k = 0; k < board.length; k++) {
 			for (int l = 0; l < board[k].length; l++) {
 				System.out.print(board[k][l] + " ");
@@ -20,6 +31,7 @@ public class CokboyutluArray {
 			}
 			System.out.print("\n");
 		}
+
 	}
 
 }
